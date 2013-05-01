aece3f8c0aaf6e2c007adb4ea153a1ff6ccf7001
kyung yeol kim <chitacan@gmail.com>
2013-05-01 18:09:49 +0900

    add template for vote btn

--- public/js/poll.js
+++ public/js/poll.js
@@ -4,44 +4,51 @@ window.addEventListener("load",function() {
   }, 1000);
 });
 
-function isEmpty(obj) {
-  for (var prop in obj) {
-    if (obj.hasOwnProperty(prop))
-      return false;
-  }
-  return true;
-}
+var title          = $('.jumbotron h2');
+var voteButtons    = $('.marketing');
+var templateSource = $('#vote-template').html();
+var voteTemplate   = Handlebars.compile(templateSource);
 
-var title = $('.jumbotron h2');
+var isDebug = document.location.hostname === 'localhost';
+var socket = io.connect(isDebug ?
+  'http://localhost:3000/client' :
+  'http://repoll.herokuapp.com/client'
+);
 
-// socket
-var socket = io.connect('http://repoll.herokuapp.com/client');
-//var socket = io.connect('http://192.168.0.102:3000/client');
+var masterNotReady = function() {
+  console.log('master not ready');
+  title.text('No Polls Yet...');
+  voteButtons.hide();
+};
+
+var masterReady = function() {
+  title.text('Pick what you want!');
+  voteButtons.append(voteTemplate({pp:['q1','q2','q3']}));
+  voteButtons.show();
+};
 
 socket.on('connect', function() {
   console.log('socket.io connected');
 });
 
-socket.on('master_ready', function(data) {
-  var parsed;
-  try {
-    parsed = JSON.parse(data);
-  } catch (e) {
-    console.log('json parse exception');
+socket.on('master_ready', function(chartData) {
+  if (chartData.chart && chartData.chart === 'none') {
+    masterNotReady();
     return;
   }
+
   console.log('master ready to vote with data!!');
-  // build views
-  title.text('Pick what you want!');
+  console.dir(chartData);
+  masterReady(chartData);
 });
 
 socket.on('master_lost', function(data) {
   console.log('master lost');
   title.text('No Polls Yet...');
-})
+});
+
 // ui event
 $('.span12 .btn').on('click', function(e) {
   e.preventDefault();
-  console.log('btn selected');
-  socket.emit('client_vote');
+  masterNotReady();
 });
